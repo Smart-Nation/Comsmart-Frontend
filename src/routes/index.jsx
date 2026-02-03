58106cb47b6e494fee977ade573d1b0a42970699
sha-riyat <sewaganse@gmail.com>
2026-02-03 13:47:51 +0000

    feat: Ajout des pages d'authentification (Login et Register)
    - Creation des pages Login et Register avec design mobile-first
    - Layout split-screen responsive pour desktop (panneau branding + formulaire)
    - Integration des icones Phosphor et du design system existant
    - Configuration des routes /login et /register

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import { Landing } from "../pages/Landing/Landing";
+import { Login } from "../pages/Auth/Login";
+import { Register } from "../pages/Auth/Register";
 
 /**
  * @type {import("react-router").RouteObject[]}
@@ -8,5 +10,13 @@ export const routes = createBrowserRouter([
   {
     index: true,
     Component: Landing,
+  },
+  {
+    path: "/login",
+    Component: Login,
+  },
+  {
+    path: "/register",
+    Component: Register,
   },
 ]);
